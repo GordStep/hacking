c97acc4267557b25c81f65dd1250ec6e076cce9b
GordStep <gordeystepushov@gmail.com>
2026-06-14 14:13:29 +0300

    Переработана механика статусов 4 -> 70+ реализация выненсена в отдлеьный файл

--- source/pkg/printer/printer.go
+++ source/pkg/printer/printer.go
@@ -4,43 +4,38 @@ import (
 	"fmt"
 	"math/rand"
 	color "source/pkg/color"
+	status "source/pkg/statuses"
 	"time"
 )
 
 func Print_lines(lines []string) {
-	status := []string{
-		"[  Load resusrs  ]",
-		"[    Stopping    ]",
-		"[  Progressing   ]",
-		"[    Starting    ]",
-	}
 	sleep_time := 200
 	for {
 		line_ind := rand.Int() % len(lines)
 		r_line := lines[line_ind]
 		col := rand.Int() % 8
-
+		fmt.Print("[")
 		switch col {
 		case 1:
-			color.White.Print(status[0])
+			color.White.Print(status.GetByNum(col))
 			sleep_time = 600
 			// time.Sleep(600 * time.Millisecond)
 
 		case 4:
-			color.Red.Print(status[1])
+			color.Red.Print(status.GetByNum(col))
 			sleep_time = 400
 			// time.Sleep(400 * time.Millisecond)
 
 		case 6:
-			color.Yellow.Print(status[2])
+			color.Yellow.Print(status.GetByNum(col))
 			sleep_time = 500
 			// time.Sleep(time.Duration(line_ind) * time.Millisecond)?
 		default:
-			color.Green.Print(status[3])
+			color.Green.Print(status.GetByNum(col))
 			sleep_time = 300
 
 		}
-		fmt.Println(" ", r_line)
+		fmt.Println("] ", r_line)
 		// fmt.Println(line_ind, r_line)
 		time.Sleep(time.Duration(sleep_time) * time.Millisecond)
 	}
